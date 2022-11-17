2b299142cd9df16fb772a0c6d25d4df2132381e3
Marek Witkowski <marek.witkowski.priv@gmail.com>
2022-11-17 21:17:43 +0100

    Update fedora37.sh

--- fedora37.sh
+++ fedora37.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 
+# Enabling the RPM Fusion repositories
+
+dnf install -y  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+
+
 # libs needed for Ruby 
 
 dnf install -y patch autoconf automake bison gcc-c++ libffi-devel libtool libyaml-devel patch readline-devel sqlite-devel zlib-devel redis
