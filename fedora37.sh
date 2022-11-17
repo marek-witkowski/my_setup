d17a0626d6f3547582195bdf41f4e73189a5e377
Marek Witkowski <marek.witkowski.priv@gmail.com>
2022-11-17 21:12:31 +0100

    Update fedora37.sh

--- fedora37.sh
+++ fedora37.sh
@@ -3,4 +3,14 @@
 
 # libs needed for Ruby 
 
-dnf install -y patch autoconf automake bison gcc-c++ libffi-devel libtool libyaml-devel patch readline-devel sqlite-devel zlib-devel
+dnf install -y patch autoconf automake bison gcc-c++ libffi-devel libtool libyaml-devel patch readline-devel sqlite-devel zlib-devel redis
+
+
+# another langs 
+
+dnf install -y go rust 
+
+
+# .... and games! ;)
+
+dnf -y install openttd
