c7c1cb44b76a90435bce689d4382606848479b18
Marek Witkowski <marek.witkowski.priv@gmail.com>
2022-11-18 21:11:12 +0100

    Create user.sh

---
+++ user.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Install RVM
+
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable
+
+source $PWD/.rvm/scripts/rvm
+
+rvm install 3.1.2
+
+gem install jekyll rails 
