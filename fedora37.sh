f662568bbf2d7d5c5478c8bf44dc5285ce49d8a7
Marek Witkowski <marek.witkowski.priv@gmail.com>
2022-11-18 20:54:54 +0100

    Update fedora37.sh

--- fedora37.sh
+++ fedora37.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
+# Change host name
+
+hostnamectl set-hostname "Baltazar"
+
+# Configure DNF for Faster Downloads of Packages
+
+echo 'max_parallel_downloads=10' >> /etc/dnf/dnf.conf
+echo 'fastestmirror=true' >> /etc/dnf/dnf.conf
+echo 'deltarpm=true' >> /etc/dnf/dnf.conf
 
 # Enabling the RPM Fusion repositories
 
 dnf install -y  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 dnf install -y  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+# Enabling Flathub repository
 
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # libs needed for Ruby 
 
@@ -15,9 +26,17 @@ dnf install -y patch autoconf automake bison gcc-c++ libffi-devel libtool libyam
 
 # another langs 
 
-dnf install -y go rust 
+dnf install -y go 
 
 
 # .... and games! ;)
 
 dnf -y install openttd
+
+
+# Install Multimedia Plugins
+
+dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+dnf install -y lame\* --exclude=lame-devel
+dnf group upgrade --with-optional Multimedia
+
