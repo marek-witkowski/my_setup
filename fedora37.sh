1ebfb555ed73a3205ddb60956b987ad9fc0c925f
Marek Witkowski <marek.witkowski.priv@gmail.com>
2022-11-29 20:20:57 +0100

    Install VSCode added.

--- fedora37.sh
+++ fedora37.sh
@@ -57,3 +57,12 @@ dnf install -y  evolution chromium fedora-chromium-config-gnome glade gitg  file
 
 # Install KDE tools and apps
 #dnf install -y krita kdenlive kdevelop 
+
+# Install VSCode
+
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+dnf install code
+
+
