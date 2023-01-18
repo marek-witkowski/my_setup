84a175577ddeab21c24037ee5053bd389f46c89f
Marek Witkowski <marek.witkowski.priv@gmail.com>
2023-01-18 15:44:10 +0100

    Update fedora37.sh

--- fedora37.sh
+++ fedora37.sh
@@ -49,7 +49,7 @@ dnf -y install openttd
 
 dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
 dnf install -y lame\* --exclude=lame-devel
-dnf group upgrade --with-optional Multimedia
+dnf group upgrade -y --with-optional Multimedia
 
 # Install GNOME tools and apps
 
