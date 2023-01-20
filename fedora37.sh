c6ef2834cec9066bfe6ba30bdc85b3f5f9c85aad
Marek Witkowski <marek.witkowski.priv@gmail.com>
2023-01-20 13:17:45 +0100

    Update fedora37.sh

--- fedora37.sh
+++ fedora37.sh
@@ -25,7 +25,7 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 
 # libs needed for Ruby 
 
-dnf install -y patch autoconf automake bison gcc-c++ libffi-devel libtool libyaml-devel patch readline-devel sqlite-devel zlib-devel redis openssl openssl-devel
+dnf install -y patch autoconf automake bison gcc-c++ libffi-devel libtool libyaml-devel patch readline-devel sqlite-devel zlib-devel redis openssl openssl-devel npm
 
 
 # Tools
