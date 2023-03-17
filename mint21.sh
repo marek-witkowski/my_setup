7e11c1a63c205b22be2ee1b5192eeb92e1b69dbb
Marek Witkowski <marek.witkowski.priv@gmail.com>
2023-03-17 18:41:05 +0100

    Update mint21.sh

--- mint21.sh
+++ mint21.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+
+
+apt-get install -y shellcheck gnupg2  g++ autoconf automake libffi-dev libgdbm-dev libncurses5-dev libsqlite3-dev libtool libyaml-dev sqlite3 zlib1g-dev, libgmp-dev libreadline-dev libssl-dev libssl-dev
+
+apt-get install -y neofetch neovim mc pandoc shellcheck mediainfo chromium pv vim libreoffice-base libreoffice-draw filezilla fonts-jetbrains-mono
+
+apt-get install -y python3-virtualenv python-is-python3 python3-pip
