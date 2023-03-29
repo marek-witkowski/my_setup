d570cc53ae126c0c2fa0469d9f50d9f4ec428a4f
Marek Witkowski <marek.witkowski.priv@gmail.com>
2023-03-29 11:46:39 +0200

    Create opensuse.sh

---
+++ opensuse.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+# Ruby
+
+zypper install patch, automake, binutils, bison, libtool, m4, make, patch, gdbm-devel, glibc-devel, libffi-devel, libopenssl-devel, readline-devel, sqlite3-devel, zlib-devel, libyaml-devel, gcc, gcc-c++
