ab72e256aa5035a3c547914414587b47d2d295d3
Marek Witkowski <marek.witkowski.priv@gmail.com>
2023-10-14 00:12:25 +0200

    Update mint21.sh

--- mint21.sh
+++ mint21.sh
@@ -5,6 +5,6 @@ apt install -y gnupg2  g++ autoconf automake libffi-dev libgdbm-dev libncurses5-
 
 apt install -y neofetch neovim mc pandoc shellcheck mediainfo chromium pv vim libreoffice-base libreoffice-draw filezilla fonts-jetbrains-mono git 
 
-apt install -y python3-virtualenv python-is-python3 python3-pip
+apt install -y python3-virtualenv python-is-python3 python3-pip libcairo2 libcairo2-dev python3-cairo python3-gi-cairo libgirepository1.0-dev
 
 apt install -y nodejs npm 
